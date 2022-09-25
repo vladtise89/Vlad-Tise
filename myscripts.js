dfd06a078bc3f4b76495d80952c973427e8cf20c
vladtise89 <113240029+vladtise89@users.noreply.github.com>
2022-09-25 17:33:05 +0300

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,33 @@
+{
+  var x = "John";
+  var y = "Doe";
+  console.log (x + y)
+
+var gender = "M";
+console.log(gender)
+
+var BMI = "BMI: 24";
+console.log(BMI)
+
+var BMI = 24;
+if (BMI > 18.5) {
+console.log ("Normal Weight")
+}
+}
+
+{
+  var x = "Mary";
+  var y = "Anne";
+  console.log (x + y)
+
+var gender = "F";
+console.log(gender)
+
+var BMI = "BMI: 30";
+console.log(BMI)
+
+var BMI = 30;
+if (BMI >= 30) {
+console.log ("Obese")
+}
+}
